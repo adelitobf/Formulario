bc8f3c7ff2a527dfcaf7d5847aaca7f0802dfa72
Adelito Farias <adelito.farias@dce.ufpb.br>
2011-10-06 01:10:29 -0300

    versao 04

--- src/br/ufpb/aps/formulario/Validador.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.aps.formulario;
-
-public interface Validador {
-
-	public void  validar(String valor) throws ValorIncompativelException;
-	
-}
--- src/br/ufpb/aps/formulario/ValorIncompativelException.java
+++
@@ -1,8 +0,0 @@
-package br.ufpb.aps.formulario;
-
-public class ValorIncompativelException extends Exception {
-
-	public ValorIncompativelException(String messagem) {
-		super(messagem);	
-	}
-}
